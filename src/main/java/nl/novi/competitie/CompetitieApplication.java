e8ebd78220cf5a764c9aa9c38d44e746759f1f19
jaapwverhees <34657795+jaapwverhees@users.noreply.github.com>
2023-05-19 12:43:19 +0200

    temp together with hanna

---
+++ src/main/java/nl/novi/competitie/CompetitieApplication.java
@@ -0,0 +1,13 @@
+package nl.novi.competitie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompetitieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompetitieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/novi/competitie/CompetitieApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.competitie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompetitieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
